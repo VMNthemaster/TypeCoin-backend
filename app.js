d57aa1aa3616c75e0641ef3fbafb9dbc8cfd1d49
VMNthemaster <varad.nadkarni09@gmail.com>
2023-02-01 23:38:45 +0530

    socket successfully sending loading state to everyone but not to the specific room

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import http from 'http'
 import cors from 'cors'
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
-import {Server} from 'socket.io'
+app.use(express.urlencoded({ extended: true }))
+import { Server } from 'socket.io'
 import { randomUUID } from 'crypto'
 import fs from 'fs'
 
@@ -15,114 +15,111 @@ app.use('/', router)
 const server = http.createServer(app)
 
 router.post('/getRoomData', (req, res) => {
-    const {username} = req.body;
-    let obj, arr, element;
+  const { username } = req.body
+  let obj, arr, element
 
-    obj = fs.readFileSync("./data.json", 'utf8')     // this is the correct way to read data
-    const parsedObj = JSON.parse(obj)
-    arr = parsedObj.data
-    element = arr[0]
+  obj = fs.readFileSync('./data.json', 'utf8') // this is the correct way to read data
+  const parsedObj = JSON.parse(obj)
+  arr = parsedObj.data
+  element = arr[0]
 
-    if(element.currentRoomCount===3){
-        // create new room
-        const roomDetails = {
-            roomId: randomUUID(),
-            currentRoomCount: 1,
-            playState: false,
-            usernames: {
-                player1: username,
-                player2: '',
-                player3: ''
-            }
-        }
-
-        arr.unshift(roomDetails)
-
-        fs.writeFileSync("data.json", JSON.stringify({data: arr}))
-
-        return res.status(200).json(roomDetails)
+  if (element.currentRoomCount === 3) {
+    // create new room
+    const roomDetails = {
+      roomId: randomUUID(),
+      currentRoomCount: 1,
+      playState: false,
+      usernames: {
+        player1: username,
+        player2: '',
+        player3: '',
+      },
     }
-    else{
-        // add the user in the room
-        element.currentRoomCount += 1
-        element.usernames[`player${element.currentRoomCount}`] = username
-        if(element.currentRoomCount === 3){
-            element.playState = true
-        }
-        arr[0] = element
 
-        fs.writeFileSync("data.json", JSON.stringify({data: arr}))
+    arr.unshift(roomDetails)
 
-        return res.status(200).json(element)
+    fs.writeFileSync('data.json', JSON.stringify({ data: arr }))
 
+    return res.status(200).json(roomDetails)
+  } else {
+    // add the user in the room
+    element.currentRoomCount += 1
+    element.usernames[`player${element.currentRoomCount}`] = username
+    if (element.currentRoomCount === 3) {
+      element.playState = true
     }
+    arr[0] = element
+
+    fs.writeFileSync('data.json', JSON.stringify({ data: arr }))
+
+    return res.status(200).json(element)
+  }
 })
 
 router.post('/getSentenceData', (req, res) => {
-    const {sentenceData, roomId} = req.body;
+  const { sentenceData, roomId } = req.body
 
-    let obj, arr, element;
+  let obj, arr, element
 
-    obj = fs.readFileSync("./data.json", 'utf8')   
-    const parsedObj = JSON.parse(obj)
-    arr = parsedObj.data
+  obj = fs.readFileSync('./data.json', 'utf8')
+  const parsedObj = JSON.parse(obj)
+  arr = parsedObj.data
 
-    const newArr = arr.map(roomDetails => {
-        if(roomDetails.roomId === roomId){
-            roomDetails.sentenceData = sentenceData
-            element = roomDetails
-        }
+  const newArr = arr.map((roomDetails) => {
+    if (roomDetails.roomId === roomId) {
+      roomDetails.sentenceData = sentenceData
+      element = roomDetails
+    }
 
-        return roomDetails;
-    })
+    return roomDetails
+  })
 
-    fs.writeFileSync("data.json", JSON.stringify({data: newArr}))
+  fs.writeFileSync('data.json', JSON.stringify({ data: newArr }))
 
-    return res.status(200).json(element)
-
+  return res.status(200).json(element)
 })
 
 router.post('/sendSentenceData', (req, res) => {
-    const {roomId} = req.body
+  const { roomId } = req.body
 
-    let obj, arr, sentenceData;
+  let obj, arr, sentenceData
 
-    obj = fs.readFileSync("./data.json", 'utf8')   
-    const parsedObj = JSON.parse(obj)
-    arr = parsedObj.data
+  obj = fs.readFileSync('./data.json', 'utf8')
+  const parsedObj = JSON.parse(obj)
+  arr = parsedObj.data
 
-    arr.map(roomDetails => {
-        if(roomDetails.roomId === roomId){
-            sentenceData = roomDetails.sentenceData
-        }
+  arr.map((roomDetails) => {
+    if (roomDetails.roomId === roomId) {
+      sentenceData = roomDetails.sentenceData
+    }
 
-        return roomDetails 
-    })
+    return roomDetails
+  })
 
-    return res.status(200).json(sentenceData)
-    
+  return res.status(200).json(sentenceData)
 })
 
-
-
 const io = new Server(server, {
-    cors: {
-        origin: 'http://localhost:3000',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
 })
 
 io.on('connection', (socket) => {
-    console.log(`user connected: ${socket.id}`)
+  console.log(`user connected: ${socket.id}`)
 
-    socket.on("join_room", (data) => {
-        console.log("room joined")
-        socket.join(`${data.roomId}`)
-    })
+  socket.on('join_room', (data) => {
+    socket.join(`${data.roomId}`)
+  })
 
-   
+  socket.on('change_loading_state', (data) => {
+    console.log(data)
+    // this is the correct way to send emit event to all users. Only socket.emit will emit event to the current connection
+    io.sockets.emit('set_loading_false', data) 
+  })
 })
 
 server.listen(5000, () => {
-    console.log("Server is running....")
+  console.log('Server is running....')
 })
